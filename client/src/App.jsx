38ea6218a931d6c57e9d95af504e42d411b63ded
sampandit553 <sameertiwari737697@gmail.com>
2026-02-16 21:39:52 +0530

    n

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
      
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/about us" element={<About />} />
         <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/terms" element={<TermsConditions />} />
         <Route path="/contact" element={<Contact />} />
